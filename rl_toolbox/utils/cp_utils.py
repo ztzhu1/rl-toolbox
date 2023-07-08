0b79b7b53b6abc287270bfe02f8d9c42b8b5801a
ztzhu <zhuzitian@zju.edu.cn>
2023-07-08 16:41:05 +0800

    Add test function

--- rl_toolbox/utils/cp_utils.py
+++ rl_toolbox/utils/cp_utils.py
@@ -1,23 +1,17 @@
-from copy import deepcopy
+import inspect
+import json
 import os
 import pathlib
 import re
 
-import json
 import keyboard
 from matplotlib import pyplot as plt
 import numpy as np
-from tqdm.auto import trange
+import pandas as pd
 
-import gymnasium as gym
 import torch
-from torch import nn
-from torch.optim import Optimizer
 
-import safetensors  # TODO
-
-from .backend import get_device
-from .network_utils import as_tensor32
+from importlib import import_module
 
 
 def search_cp_file_name(cp_dir, epochs=None):
@@ -43,59 +37,95 @@ def search_cp_file_name(cp_dir, epochs=None):
     return os.path.join(cp_dir, f"epochs_{epochs}.pt")
 
 
-def save_checkpoint(cp_dir, epochs, config, args_in_name=None, **params_to_save):
-    config = deepcopy(config)
-    config["epochs"] = epochs
-    cp_dir = os.path.abspath(cp_dir)
-    if args_in_name is not None:
-        for i in args_in_name:
-            assert i in config
-            cp_dir += "-%s_%s" % (i, str(config[i]))
+def save_checkopoint(agent, saved_attr_names):
+    cp_dir = agent._cp_dir
+    if os.path.exists(cp_dir):
+        raise FileExistsError(f"Directory {cp_dir} already exists!")
+    cfg_path = os.path.join(cp_dir, f"config.pt")
+    log_path = os.path.join(cp_dir, f"log.csv")
+    data_path = os.path.join(cp_dir, f"epochs_{agent._curr_epoch}.pt")
 
-    data = {"config": config}
-    data.update(params_to_save)
+    pathlib.Path(agent._cp_dir).mkdir(parents=True, exist_ok=True)
 
-    filename = os.path.join(cp_dir, f"epochs_{epochs}.pt")
-    if os.path.exists(filename):
-        print("\x1b[1;33mWarning: Overwriting %s!\x1b[0m" % (filename))
+    if not os.path.exists(cfg_path):
+        torch.save(agent._cfg, cfg_path)
+    agent._log.to_csv(log_path, index=False)
 
-    pathlib.Path(cp_dir).mkdir(parents=True, exist_ok=True)
-    torch.save(data, filename)
+    class_name = type(agent).__name__
+    class_abs_path = inspect.getfile(agent.__class__)
+    _, class_filename = os.path.split(class_abs_path)
+    class_filename_wo_ext = os.path.splitext(class_filename)[0]
+    entry_point = class_filename_wo_ext + "." + class_name
+    data = dict(entry_point=entry_point)
+    for name in saved_attr_names:
+        attr = getattr(agent, name)
+        if hasattr(attr, "state_dict"):
+            attr = attr.state_dict()
+            name += "-state_dict"
+        data[name] = attr
+    if agent._save_buf:
+        data["buf"] = agent._buf
+    torch.save(data, data_path)
 
 
-def load_checkpoint(cp_dir, epochs=None, device=None):
-    filename = search_cp_file_name(cp_dir, epochs)
-    if filename is None:
-        raise FileNotFoundError(f"Cannont find the checkpoint in {cp_dir}!")
-    if device is not None:
-        device = torch.device(device)
-    data = torch.load(filename, map_location=device)
-    return data
-
-
-def test_checkpoint(cp_dir, epochs=None, run_epochs=10, **env_kwargs):
-    device = get_device()
-    model = load_checkpoint(cp_dir, epochs, device)
-    cfg = model._cfg
+def load_checkpoint(
+    class_, cp_dir, epochs=None, render_mode=None, human_render_hook=None, **env_kwargs
+):
+    cfg = torch.load(os.path.join(cp_dir, "config.pt"), map_location="cpu")
+    log = pd.read_csv(os.path.join(cp_dir, "log.csv"))
+    data = torch.load(search_cp_file_name(cp_dir, epochs), map_location="cpu")
     print(json.dumps(cfg, sort_keys=True, indent=4, default=str))
+    cfg["init_stats_param"] = data["_env-state_dict"]
 
-    cfg["env_kwargs"]["render_mode"] = "human"
-    cfg["env_kwargs"].update(env_kwargs)
-    cfg["epochs"] = run_epochs * cfg["steps_per_epoch"]
-    cfg["steps_per_epoch"] = 1
-    cfg["init_stats_param"] = model._env.state_dict()
-    if cfg["max_steps_per_traj"] < 0:
-        cfg["max_steps_per_traj"] = 1000
+    agent = class_(cp_dir=None, env_name=data["_env_name"], **cfg)
+    agent._curr_epoch = data["_curr_epoch"]
+    agent._vis_value_names = data["_vis_value_names"]
+    agent._log = log[:epochs]
 
-    model._init_env()
-    model._init_collector()
+    if render_mode is not None:
+        # testing mode
+        cfg = agent._cfg
+        assert render_mode == "human"
+        cfg["env_kwargs"].update(env_kwargs)
+        cfg["env_kwargs"]["render_mode"] = render_mode
+        cfg["epochs"] = 10 * cfg["steps_per_epoch"]
+        cfg["steps_per_epoch"] = 1
+        if cfg["max_steps_per_traj"] < 0:
+            cfg["max_steps_per_traj"] = 1000
+        if human_render_hook is not None:
+            human_render_hook(cfg)
 
-    for data in model._collector:
-        if data["next"]["done"] or data["next"]["truncated"]:
+    agent._init_all()
+    agent._update_vis()
+
+    for key in data:
+        if key.endswith("-state_dict"):
+            agent.__dict__[key[:-11]].load_state_dict(data[key])
+
+    if render_mode != "human" and "buf" in data:
+        agent._buf = data["_buf"]
+
+    return agent
+
+
+def test_checkpoint(cp_dir, epochs=None, **env_kwargs):
+    data = torch.load(search_cp_file_name(cp_dir, epochs), map_location="cpu")
+    agent_file, agent_class = data["entry_point"].split(".")
+    m = import_module(f"rl_toolbox.agents.{agent_file}")
+    class_ = getattr(m, agent_class)
+
+    agent = class_.from_checkpoint(cp_dir, epochs, "human", **env_kwargs)
+
+    for data in agent._collector:
+        data_next = data["next"]
+        if data_next["done"] or data_next["truncated"]:
             print(
                 f"step_count: %d, traj_reward: %lf"
-                % (data["next"]["step_count"].cpu().item(), data["episode_reward"].cpu().item())
+                % (
+                    data_next["step_count"].cpu().item(),
+                    data_next["episode_reward"].cpu().item(),
+                )
             )
         if keyboard.is_pressed("Esc"):
             break
-    plt.close(model._fig)
+    plt.close(agent._fig)
